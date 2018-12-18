24ad00b511461d8d50b73239aca96250e2c2387c
Brandon <brandoncrichie@gmail.com>
2018-12-18 15:34:35 -0800

    Assignment 1 Express Finished

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,17 +5,25 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 //title, test suite to match HTTP verb and route
     describe("GET /", () => {
-
 // requesting the serve, code 200 = successful.    
         it("should return status code 200", (done) => {
-
 //send GET base URL
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
 //passing done() to let Jasmine test completed
                 done();
             });
         });
     });
+
+
+describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+        request.get(base + "marco", (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(body).toBe("polo");
+            done();
+        });
+    });
+  });
 });
